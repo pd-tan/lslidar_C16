b9647892cad93bd488f66a24c796e80748e35dbd
FXCarl <fxcarl@qq.com>
2019-08-20 10:14:29 +0800

    rebase to uplink

--- lslidar_c16_decoder/src/lslidar_c16_decoder_nodelet.cpp
+++ lslidar_c16_decoder/src/lslidar_c16_decoder_nodelet.cpp
@@ -31,6 +31,5 @@ void LslidarC16DecoderNodelet::onInit() {
 
 } // end namespace lslidar_c16_decoder
 
-// Register this plugin with pluginlib.  Names must match nodelet_lslidar.xml.
-// parameters are: class type, base class type
-PLUGINLIB_EXPORT_CLASS(lslidar_c16_decoder::LslidarC16DecoderNodelet, nodelet::Nodelet);
+PLUGINLIB_DECLARE_CLASS(lslidar_c16_decoder, LslidarC16Nodelet,
+    lslidar_c16_decoder::LslidarC16DecoderNodelet, nodelet::Nodelet);
